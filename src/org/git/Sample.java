9d096cc3c264f1cff69f67e97810ff1ce8023d6d
SujiKarthick <thanvesh2402@gmail.com>
2022-11-23 14:07:49 +0530

    Code Added

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -6,9 +6,15 @@ public class Sample {
 		System.out.println(a + b);
 	}
 
+	private void sub() {
+		int a = 20, b = 10;
+		System.out.println(a - b);
+	}
+
 	public static void main(String[] args) {
 		Sample a = new Sample();
 		a.add();
+		a.sub();
 	}
 
 }
